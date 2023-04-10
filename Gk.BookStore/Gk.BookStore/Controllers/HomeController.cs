3948006fc589ca17cd99e99feaf2469e8a7e9a5e
Urvish Vaghasiya <108147458+webbyurvish@users.noreply.github.com>
2023-04-10 20:13:17 +0530

    Added MVC to the application

---
+++ Gk.BookStore/Gk.BookStore/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gk.BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "WebGentle";
+        }
+    }
+}
--- Gk.BookStore/Gk.BookStore/Program.cs
+++ Gk.BookStore/Gk.BookStore/Program.cs
@@ -9,12 +9,24 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
+
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapGet("/", async context =>
+//    {
+//        await context.Response.WriteAsync("Hello World");
+//    });
+//});
 
 app.MapControllerRoute(
     name: "default",
